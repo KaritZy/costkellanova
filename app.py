c0122ae0b70c92819b453ada8ec1c31f65f18438
Ricardo <2023276001@uteq.edu.mx>
2024-09-25 01:16:56 -0600

    Corrección de campo cancelado

--- app.py
+++ app.py
@@ -226,6 +226,7 @@ def cancelar_cotizacion(folio):
         cotizacion.respondido = False  # Desmarcar como respondido
         cotizacion.cancelado = True  # Marcar como cancelado
         db.session.commit()
+        print(f'Cotización {folio} ha sido cancelada: {cotizacion.cancelado}')  # Imprimir en consola
         flash(f'Cotización con folio {folio} ha sido cancelada.')
     else:
         flash('Cotización no encontrada o no está en estado cotizado.')
